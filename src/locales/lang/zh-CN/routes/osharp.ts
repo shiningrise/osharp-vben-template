33bf716e55d1ff4ece95cf2e5d16f50f913e1005
郭明锋 <gmf520@yeah.net>
2022-08-15 11:52:16 +0800

    wip: 添加多语言支持

---
+++ src/locales/lang/zh-CN/routes/osharp.ts
@@ -0,0 +1,31 @@
+// -----------------------------------------------------------------------
+//  <once-generated>
+//     这个文件只生成一次，再次生成不会被覆盖。
+//  </once-generated>
+//
+//  <copyright file="src/locales/lang/zh_CN/routers/osharp.ts" company="LiuliuSoft">
+//      OSHARP.ORG@2021
+//  </copyright>
+//  <site>https://www.osharp.org</site>
+//  <last-editor>郭明锋</last-editor>
+// -----------------------------------------------------------------------
+export default {
+  system: '系统管理',
+  identity: {
+    identity: '身份认证',
+    user: '用户管理',
+    role: '角色管理',
+    userRole: '用户角色管理',
+  },
+  auth: {
+    auth: '权限授权',
+    module: '模块管理',
+    function: '功能管理',
+    entityInfo: '实体管理',
+  },
+  infos: {
+    infos: '信息',
+    article: '文章管理',
+    message: '站内信管理',
+  },
+};
